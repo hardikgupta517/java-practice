05a5e3e0a923f088983fb71af1486d0e1308fa01
hardikgupta517 <hardikguptason@gmail.com>
2026-06-02 12:20:27 +0530

    Added Java variables practice program

---
+++ Basics/variables.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int myInt = 42;
+        double myDouble = 3.14159; 
+        char myChar = 'A';
+        boolean myBoolean = true;
+        String myString = "Hello, Java!";
+      
+        System.out.println("Integer (int): " + myInt);
+        System.out.println("Double (double): " + myDouble);
+        System.out.println("Character (char): " + myChar);
+        System.out.println("Boolean (boolean): " + myBoolean);
+        System.out.println("String (String): " + myString);
+    }
+}
