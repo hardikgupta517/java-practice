1752129c4fc2ed3a496e46da1ff3f63058b59f02
hardikgupta517 <hardikguptason@gmail.com>
2026-06-04 11:52:04 +0530

    Added Java loops practice

---
+++ Basics/Loops.java
@@ -0,0 +1,15 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            System.out.println(i);
+        }
+        int j=1;
+        while (j<=5){
+            System.out.println("Hello");
+            j++;
+        }
+        for (int k=10; k>=1;k--){
+            System.out.println(k);
+        }
+    }
+}
