1650f89113ac74b0793a6e4ff195d97c6ff6965b
hardikgupta517 <hardikguptason@gmail.com>
2026-06-08 17:04:05 +0530

    Add multiple pattern printing methods in AdvancedPatterns

---
+++ Basics/AdvancedPatterns.java
@@ -0,0 +1,47 @@
+//Number Pyramid
+public class AdvancedPatterns {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i=1; i<=n; i++){
+            for ( int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for ( int k=1; k<=(2*i-1); k++){
+                System.out.print(i);
+            }System.out.println(" ");
+        }
+    }
+}
+
+//Solid Rhombus
+public class AdvancedPatterns {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i=1; i<=n; i++){
+            for ( int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for ( int k=1; k<=n; k++){
+                System.out.print("*");
+            }System.out.println(" ");
+        }
+    }
+}
+
+//Butterfly (Upper Half)
+public class AdvancedPatterns {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i=1; i<=n; i++){
+            for ( int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for ( int k=1; k<=2*(n-i); k++){
+                System.out.print(" ");
+            }
+            for ( int j=1; j<=i; j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
